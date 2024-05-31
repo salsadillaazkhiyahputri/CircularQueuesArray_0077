0712ab787830b3e8db7325a4cd6f77630de332b6
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-31 14:59:05 +0700

    menampilkan semua elemen dari depan sampai belakang

--- CircularQueuesArray_0077.cpp
+++ CircularQueuesArray_0077.cpp
@@ -70,4 +70,14 @@ public:
             return;
         }
 
-        
+        cout << "\nElements in the queue are...\n";
+
+        // jika FRONT_position <= REAR_position. iterasi dari FRONTR hingga REAR
+        if (FRONT_position <= REAR_position){
+            while (FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+        }
+    }
+}
