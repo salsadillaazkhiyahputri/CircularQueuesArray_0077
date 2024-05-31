adcf54c57e92fe0953b808cf4267286ec9eeb87e
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-05-31 14:37:25 +0700

    mendeklarasikan class queues

---
+++ CircularQueuesArray_0077.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Queues {
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
+
+public:
+    Queues(){
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert(){
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        //cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout <<"\nQueue overflow\n";
+            return;
+        }
+    }
+}
